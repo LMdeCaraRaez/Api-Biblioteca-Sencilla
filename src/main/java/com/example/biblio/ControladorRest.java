a0e79b3f646cc7c723775933458af84d9e17746c
luismi <luismigueldecararaez@gmail.com>
2025-07-16 19:42:44 +0200

    Añadido thymeleaf, cambios menores en el controlador

--- src/main/java/com/example/biblio/ControladorRest.java
+++ src/main/java/com/example/biblio/ControladorRest.java
@@ -1,14 +1,32 @@
 package com.example.biblio;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// @RestController
+//public class ControladorRest {
+//
+//
+//    @GetMapping("/")
+//    public String inicio() {
+//
+//
+//        return "indice";
+//    }
+//}
+
+@Controller // Se usa para que las plantillas funcionen
 public class ControladorRest {
-    
+
 
     @GetMapping("/")
-    public String inicio() {
-        return "Hola Mundo!!!";
+    public String inicio(Model model) {
+        String textoSaludo = "Buenas Tardes, bienvenido a mi proyecto";
+
+        model.addAttribute("textoSaludo", textoSaludo); // Con esto mandamos los atributos del controlador a la plantilla
+
+        // En el return si ponemos el nombre de nuestra plantilla, esta será lo que se muestra
+        return "indice";
     }
 }
